57fc51a399cf5a61f84c5d5654da3d5b5ff3224a
AlexSudnik <b.f.pierce@icloud.com>
2025-11-20 17:47:42 +0300

    bug fixes, delete unused imports

--- src/test/java/SauceDemoTest.java
+++ src/test/java/SauceDemoTest.java
@@ -1,5 +1,4 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertTrue;
 public class SauceDemoTest extends BaseTest{
